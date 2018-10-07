b4d67aa502dc8c365e416ed1c2110ffb818d5dd7
Thiago Brito <thyago_bryto@hotmail.com>
2018-10-07 16:18:52 -0300

    first

---
+++ SelectITsolutionT1/SelectITsolutionT1/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Brito\Documents\Thiago Brito\Thiago Facu\Python\SelectITSolutionT1\SelectITsolutionT1\SelectITsolutionT1\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
